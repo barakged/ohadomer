d4d3f9c4fa613443eb4e68676359857635eb7764
barak@cynerio.co <barakged85@gmail.com>
2022-11-22 17:48:13 +0200

    asd

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+const path = require('path');
+
+
+// create an express app
+const express = require("express")
+const app = express()
+
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname+'/app/index.html'))
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
