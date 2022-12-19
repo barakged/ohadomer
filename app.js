8521428db5bc2a999b423016d4be0c3be5145d52
barak@cynerio.co <barakged85@gmail.com>
2022-12-19 13:20:20 +0200

    asd

--- app.js
+++ app.js
@@ -6,21 +6,25 @@ const path = require('path');
 // create an express app
 const express = require("express")
 const app = express()
-app.enable('trust proxy')
 
 // use the express-static middleware
 app.use(express.static("public"))
-app.use(function(request, response, next) {
-    if (process.env.NODE_ENV != 'development' && !request.secure) {
-       return response.redirect("https://" + request.headers.host + request.url);
-    }
-    next();
-})
+
 // define the first route
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname+'/app/index.html'))
 })
-
+//http to https redirects
+app.enable('trust proxy');
+app.use((req,res,next)=>{
+    console.log('hereeee')
+    if(rec.secure){
+        next();
+    }else {
+        console.log('hereeee222')
+        res.redirect('https://www.ohadoner.co.il'+req.url);
+    }
+});
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
