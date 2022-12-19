64003c0793b50637169c9cc62e3f92b35828cf1a
barak@cynerio.co <barakged85@gmail.com>
2022-12-19 13:10:02 +0200

    asd

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const path = require('path');
 // create an express app
 const express = require("express")
 const app = express()
+app.enable('trust proxy')
 
 // use the express-static middleware
 app.use(express.static("public"))
-
+app.use((req, res, next) => {
+    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+})
 // define the first route
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname+'/app/index.html'))
