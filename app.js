f8dd82c3d699392cc1ccc3106bba8d8384fefb67
barak@cynerio.co <barakged85@gmail.com>
2022-12-19 13:48:24 +0200

    asd

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 
 
-const path = require('path');
+// const path = require('path');
 
 
-// create an express app
-const express = require("express")
-const app = express()
-app.all('*', function(req, res, next){
-    console.log('req start: ',req.secure, req.hostname, req.url, app.get('port'));
-    if (req.secure) {
-        return next();
-    }
+// // create an express app
+// const express = require("express")
+// const app = express()
 
-    res.redirect('https://'+req.hostname + ':' + app.get('secPort') + req.url);
-});
-// use the express-static middleware
-app.use(express.static("public"))
+// // use the express-static middleware
+// app.use(express.static("public"))
 
-// define the first route
-app.get("/", function (req, res) {
+// // define the first route
+// app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname+'/app/index.html'))
+// })
+
+// // start the server listening for requests
+// app.listen(process.env.PORT || 3000, 
+// 	() => console.log("Server is running..."));
+
+
+var express = require('express');
+var enforce = require('express-sslify');
+
+var app = express();
+
+// put it as one of the first middlewares, before routes
+app.use(enforce.HTTPS()); 
+
+// handling your static files just like always
+app.use(express.static(__dirname + '/public'));
+
+// handling requests to root just like always
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname+'/app/index.html'))
-})
+});
 
-// start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.listen(3000);
