5d639ea9c0a77d9c67bf7f98bb379337a99d83d0
barak@cynerio.co <barakged85@gmail.com>
2022-12-19 13:54:10 +0200

    asd

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const path = require('path');
 const express = require("express")
 const app = express()
 
+app.use(function(request, response, next) {
+    if(req.headers["x-forwarded-proto"] === "https"){
+        // OK, continue
+        return next();
+      };
+      res.redirect('https://'+req.hostname+req.url);
+})
+
 // use the express-static middleware
 app.use(express.static("public"))
 
