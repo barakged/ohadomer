cd561d28451e7623fb861e407f18deda5c4db813
barak@cynerio.co <barakged85@gmail.com>
2022-12-19 13:15:29 +0200

    asd

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ app.enable('trust proxy')
 
 // use the express-static middleware
 app.use(express.static("public"))
-app.use((req, res, next) => {
-    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+app.use(function(request, response, next) {
+    if (process.env.NODE_ENV != 'development' && !request.secure) {
+       return response.redirect("https://" + request.headers.host + request.url);
+    }
+    next();
 })
 // define the first route
 app.get("/", function (req, res) {
